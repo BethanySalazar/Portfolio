074d8985355fa5d1489796cb7778c6b51a079061
Bethany <bethanysalazar2007@hotmail.com>
2023-06-07 23:43:59 -0500

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -1,57 +1,88 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
+function App () {
   const [inspo, setInspo] = useState(['https://katielizlewis.com/'])
   const [tabs, setTabs] = useState([
     { id: 'home', label: 'Home' },
     { id: 'work', label: 'Work' },
     { id: 'aboutme', label: 'About me' },
     { id: 'passions', label: 'Passions' },
-    { id: 'contactme', label: 'Contact' },
+    { id: 'contactme', label: 'Contact' }
   ])
 
   const [projects, setProjects] = useState([
     {
       id: 'miriamspartyland',
       label: 'Miriams Partyland',
-      projecttype: ['App', 'Branding', 'Campaign', 'Email', 'Illustration', 'Print', 'UX', 'Web']
+      projecttype: [
+        'App',
+        'Branding',
+        'Campaign',
+        'Email',
+        'Illustration',
+        'Print',
+        'UX',
+        'Web'
+      ]
     },
     { id: 'eurodite', label: 'Eurodite' },
     { id: 'datamanagement', label: 'Allergy Database' }
   ])
 
-  const [contactInfo, setContactInfo] = useState({
-    name: 'Bethany Salazar',
-    email: 'bethanysalazar2007@hotmail.com',
-    phonenumber: '(512) 363-6887',
-    // linkedlin: { icon: '', label: 'LinkedIn' }
-  })
-
+  const [contactInfo, setContactInfo] = useState([
+    { id: '', label: 'Name', icon: '', value: 'Bethany Salazar' },
+    {
+      id: '',
+      label: 'Email',
+      icon: '',
+      value: 'bethanysalazar2007@hotmail.com'
+    },
+    { id: '', label: 'Phone number', icon: '', value: '(512) 363-6887' },
+    { id: '', label: 'LinkedIn', icon: '', value: 'LinkedIn' }
+  ])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{
-          border: '1px solid red',
-          display: 'flex',
-          justifyContent: 'space-between'
-        }}>
+    <div className='App'>
+      <header className='App-header'>
+        <div
+          style={{
+            border: '1px solid red',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
           <div>Bethany Salazar</div>
           <div style={{ border: '1px solid red', display: 'flex' }}>
-            {tabs.map(tab => <div
-              key={tab.id}
-              style={{ padding: '0px 0px 0px 10px' }}
-            >
-              {tab.label}
-            </div>)}
+            {tabs.map(tab => (
+              <div key={tab.id} style={{ padding: '0px 0px 0px 10px' }}>
+                {tab.label}
+              </div>
+            ))}
           </div>
         </div>
-        <div style={{ border: '1px solid red', display: 'flex', height: 'fit-content', flexDirection: 'column' }}>
+        <div
+          style={{
+            border: '1px solid red',
+            display: 'flex',
+            height: 'fit-content',
+            flexDirection: 'column'
+          }}
+        >
           hello
-          <div style={{ border: '1px solid red', display: 'flex', height: 'fit-content', background: '', margin: '20px 20px 20px 20px ' }}>
+          <div
+            style={{
+              border: '1px solid red',
+              display: 'flex',
+              height: 'fit-content',
+              background: '',
+              margin: '20px 20px 20px 20px '
+            }}
+          >
             <div style={{ display: 'flex', flexDirection: 'column' }}>
-              {Object.entries(contactInfo).map((field, index) => <div key={index + '_' + field[0]}>{field[1]}</div>)}
+              {Object.entries(contactInfo).map((field, index) => (
+                <div key={index + '_' + field[0]}>{field[1]}</div>
+              ))}
             </div>
             <div></div>
           </div>
@@ -61,4 +92,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
